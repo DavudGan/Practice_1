e26661fc1813f6e34cb123eacf44472e96b48fb5
davud <zu2015@mail.ru>
2023-04-18 19:22:29 +0500

    website translation_commit_2

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
-
 jQuery(document).ready(function($){
+    let currentLanguage = 'en'
     $('.language-change-button').click(function() {
         for (let key in langArr)
         {
-            $('.'+key).text(langArr[key]['ru'])
+            $('#'+key).text(langArr[key][currentLanguage])
         }
+        if (currentLanguage === "en"){
+            currentLanguage = "ru"
+        }else currentLanguage = "en"
     })
 })
