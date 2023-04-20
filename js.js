13691458ed6045082c31a2d24572cd4d93c18455
davud <zu2015@mail.ru>
2023-04-20 12:12:41 +0500

    fix_menu-list_(css)

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
 jQuery(document).ready(function($){
     let currentLanguage = 'ru'
     $('.language-change-button').click(function() {
+        if (currentLanguage === "en"){
+            $('.menu-list').css({'gap':'156px'})
+        }else $('.menu-list').css({'gap':'146px'})
+        
         for (let key in langArr)
         {
             $('#'+key).text(langArr[key][currentLanguage])
         }
+
         if (currentLanguage === "en"){
             currentLanguage = "ru"
-        }else currentLanguage = "en"
+        }else currentLanguage = "en" 
     })
 })
