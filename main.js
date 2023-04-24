e0dd18f7e82968c046fd342638b19342a3e094ac
davud <zu2015@mail.ru>
2023-04-24 20:52:06 +0500

    fix_bag_3

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function($){
     let currentLanguage = 'ru'
     $('.language-change-button').click(function() {
-        if (currentLanguage === "en"){
-            $('.menu-list').css({'gap':'156px'})
-            $('.my-info').css({'gap':'212px'})
-        }else ($('.menu-list').css({'gap':'146px'}), $('.my-info').css({'gap':'226px'}))
-        
+        // if (currentLanguage === "en"){
+        //     $('.menu-list').css({'gap':'156px'})
+        //     $('.my-info').css({'gap':'212px'})
+        // }else ($('.menu-list').css({'gap':'146px'}), $('.my-info').css({'gap':'226px'}))
+
+        // $("body").addClass("data-lang-" + currentLanguage)
+
+        $("body").attr("data-lang", currentLanguage)
         for (let key in langArr)
         {
             $('#'+key).text(langArr[key][currentLanguage])
@@ -14,5 +17,9 @@ jQuery(document).ready(function($){
         if (currentLanguage === "en"){
             currentLanguage = "ru"
         }else currentLanguage = "en" 
+
+        // $("body").attr("lang", currentLanguage)
+        console.log($("body").attr("lang"))
+
     })
 })
